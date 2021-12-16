61eafe0381efe6051e3aff0c2a9fcb74f77ae38d
poohbearr <syb369@naver.com>
2021-12-16 18:37:38 +0900

    ContactController(Refactoring01)

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -22,45 +22,58 @@ public class ContactController{
 
   @RequestMapping("contact/add")
   public Object add(String name, String email, String tel, String company) {
-    String contact = name + "," + email + "," + tel + "," + company;
-    contacts[size++] = contact;
+    contacts[size++] = createCSV(name, email, tel, company);
     return size;
   }
 
   @RequestMapping("/contact/get")
   public Object get(String email) {
-    for (int i = 0; i < size; i++) {
-      if (contacts[i].split(",")[1].equals(email))
-        return contacts[i];
-    }
-    return "";
+    int index = indexOf(email);
+    if (index == -1)
+      return "";
+    return contacts[index];
   }
 
   @RequestMapping("/contact/update")
   public Object update(String name, String email, String tel, String company) {
-    String contact = name + "," + email + "," + tel + "," + company;
-    for (int i = 0; i < size; i++) {
-      if (contacts[i].split(",")[1].equals(email)) {
-        contacts[i] = contact;
-        return 1;
-      }
-    }
-    return 0;
+    int index = indexOf(email);
+    if (index == -1)
+      return 0;
+    contacts[index] = createCSV(name, email, tel, company);
+    return 1;
   }
 
   @RequestMapping("/contact/delete")
   public Object delete(String email) {
+    int index = indexOf(email);
+    if (index == -1)
+      return 0;
+
+    remove(index);
+    return 1;
+  }
+
+  String createCSV(String name, String email, String tel, String company) {
+    return name + "," + email + "," + tel + "," + company;
+  }
+
+  int indexOf(String email) {
     for (int i = 0; i < size; i++) {
       if (contacts[i].split(",")[1].equals(email)) {
 
-        for (int j = i + 1; j < size; j++) {
-          contacts[j - 1] = contacts[j];
-        }
-        size--;
         return 1;
       }
     }
-    return 0;
+    return -1;
+  }
+
+  String remove(int index) {
+    String old = contacts[index];
+    for (int i = index + 1; i < size; i++) {
+      contacts[i - 1] = contacts[i];
+    }
+    size--;
+    return old;
   }
 }
 
