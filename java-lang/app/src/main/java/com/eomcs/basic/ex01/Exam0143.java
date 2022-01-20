159f44aff059e88b03d745ece54289908a3c39bd
poohbearr <syb369@naver.com>
2022-01-20 23:08:45 +0900

    22.01.20 serialize/ basic

--- java-lang/app/src/main/java/com/eomcs/basic/ex01/Exam0143.java
+++ java-lang/app/src/main/java/com/eomcs/basic/ex01/Exam0143.java
@@ -27,7 +27,7 @@ public class Exam0143 {
 
     // s1, s2의 Score 인스턴스는 서로 다른 인스턴스이다. 
     System.out.println(s1 == s2);
-    
+
     // Object에서 상속 받아 사용하는 hashCode()는 
     // 기본적으로 인스턴스 마다 고유의 값을 리턴한다.
     // 그래서 다음 출력은 비록 같은 값을 갖고 있다하더라도 
