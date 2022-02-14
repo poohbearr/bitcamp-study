98fd95544cf41c220a540c67c8573374e2f654ac
poohbearr <syb369@naver.com>
2022-02-14 22:12:44 +0900

    22.02.14

---
+++ java-lang/app/src/main/java/com/eomcs/algorithm/data_structure/linkedlist2/step6/Iterator.java
@@ -0,0 +1,8 @@
+package com.eomcs.algorithm.data_structure.linkedlist2.step6;
+
+// 컬레션의 값을 조회하는 객체 사용법
+//
+public interface Iterator {
+  boolean hasNext();
+  Object next();
+}
---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/h/test/Iterator.java
@@ -0,0 +1,8 @@
+package com.eomcs.oop.ex11.h.test;
+
+// 컬레션의 값을 조회하는 객체 사용법
+//
+public interface Iterator {
+  boolean hasNext();
+  Object next();
+}
