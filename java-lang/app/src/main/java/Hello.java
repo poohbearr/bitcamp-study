0a6c5db03f638d164051391e71bf4e76da46b040
poohbearr <syb369@naver.com>
2022-02-28 23:13:01 +0900

    ..

--- java-lang/app/src/main/java/Hello.java
+++ java-lang/app/src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
   public static void main(String[] args) throws Exception {
     System.out.println(System.getProperty("java.class.path"));
-    System.out.println("Hello2");
+    System.out.println("Hello");
 
   }
 
