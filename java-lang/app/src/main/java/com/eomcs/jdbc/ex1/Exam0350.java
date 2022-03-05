6652c3a1387db923a3e73dbace09528201d95f7f
poohbearr <syb369@naver.com>
2022-03-05 22:52:36 +0900

    22.03.04

--- java-lang/app/src/main/java/com/eomcs/jdbc/ex1/Exam0350.java
+++ java-lang/app/src/main/java/com/eomcs/jdbc/ex1/Exam0350.java
@@ -8,7 +8,7 @@ public class Exam0350 {
   public static void main(String[] args) throws Exception {
     try (
         java.sql.Connection con = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
+            "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
         java.sql.Statement stmt = con.createStatement();
         ) {
 
@@ -17,7 +17,7 @@ public class Exam0350 {
       // => 리턴 값: 변경된 레코드의 개수이다.
       int count = stmt.executeUpdate( 
           "update x_board set view_count = view_count + 1"
-              + " where board_id = 4");
+              + " where board_id < 100");
       System.out.printf("%d 개 변경 성공!", count);
     }
   }
