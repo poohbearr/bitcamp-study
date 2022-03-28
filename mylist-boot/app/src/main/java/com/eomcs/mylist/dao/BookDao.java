924a7835d5557b19b870a904f4fda830a2080e54
poohbearr <syb369@naver.com>
2022-03-28 16:59:39 +0900

    22.03.28 img-upload/lombok

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/BookDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/BookDao.java
@@ -7,20 +7,26 @@ import com.eomcs.mylist.domain.Book;
 @Mapper // => Mybatis는 다음 인터페이스의 구현체를 자동으로 생성한다.
 public interface BookDao {
 
-  // 인터페이스는 객체의 메서드 호출 규칙을 정의하는 것이기 때문에
-  // 메서드를 작성할 때 메서드 몸체(method body)를 작성하지 말아야 한다.
-  // 메서드 바디가 없는 메서드를 "추상 메서드(abstract method)"라 부른다.
-
   int countAll();
 
   List<Book> findAll();
 
-  void insert(Book book);
+  int insert(Book book);
 
   Book findByNo(int no);
 
   int update(Book book);
 
   int delete(int no);
-
 }
+
+
+
+
+
+
+
+
+
+
+
